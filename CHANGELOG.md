a2d28a9c1de0baee2621cdb7b89bb870a5c4258d
Thorben Schröder <stillepost@gmail.com>
2012-08-21 18:32:48 +0200

    Bumps to 0.0.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# 0.0.2
+
+* Adds a relationship validation mechanism
+
 # 0.0.1
 
 The start.
--- lib/graph-backend/version.rb
+++ lib/graph-backend/version.rb
@@ -1,5 +1,5 @@
 module Graph
   module Backend
-    VERSION = "0.0.1"
+    VERSION = "0.0.2"
   end
 end
